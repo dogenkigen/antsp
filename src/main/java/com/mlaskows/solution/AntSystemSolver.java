49eb955f76932bfa22d0982a0bf0c130346deef6
Maciej Laskowski <mlaskowsk@gmail.com>
2017-06-25 09:58:59 +0200

    decisionRule impl

--- src/main/java/com/mlaskows/solution/AntSystemSolver.java
+++ src/main/java/com/mlaskows/solution/AntSystemSolver.java
@@ -19,18 +19,40 @@ public class AntSystemSolver implements Solver {
     private final AcoConfig config;
     private final MatricesHolder matrices;
     private final List<Ant> ants;
-    private final SplittableRandom splittableRandom = new SplittableRandom();
+    private final SplittableRandom random = new SplittableRandom();
+    private final double[][] choicesInfo;
+    private int problemSize;
 
     public AntSystemSolver(AcoConfig config, MatricesHolder matrices) {
         this.config = config;
         this.matrices = matrices;
+        this.problemSize = matrices.getProblemSize();
+        // FIXME constructor shouldn't be so heavy?
         this.ants = getRandomPlacedAnts();
+
+        choicesInfo = getChoicesInfo();
+    }
+
+    private double[][] getChoicesInfo() {
+        // TODO move to matrices holder?
+        final double[][] doubles = new double[problemSize][problemSize];
+        for (int i = 0; i < problemSize; i++) {
+            for (int j = i; j < problemSize; j++) {
+                final double choice = Math.pow(matrices.getPheromoneMatrix()[i][j], config
+                        .getPheromoneImportance()) * Math.pow(matrices
+                        .getHeuristicInformationMatrix()[i][j], config
+                        .getHeuristicImportance());
+                doubles[i][j] = choice;
+                doubles[j][i] = choice;
+            }
+        }
+        return doubles;
     }
 
     private List<Ant> getRandomPlacedAnts() {
-        return splittableRandom.ints(0, matrices.getProblemSize())
+        return random.ints(0, problemSize)
                 .limit(config.getAntsCount())
-                .mapToObj(position -> new Ant(matrices.getProblemSize(), position))
+                .mapToObj(position -> new Ant(problemSize, position))
                 .collect(Collectors.toList());
     }
 
@@ -48,13 +70,36 @@ public class AntSystemSolver implements Solver {
     }
 
     private void constructSolution() {
-        IntStream.range(0, matrices.getProblemSize())
+      /*  IntStream.range(0, problemSize)
                 .forEach(step -> ants.stream()
-                        .forEach(ant -> decisionRule(ant, step)));
+                        .forEach(ant -> decisionRule(ant)));*/
+        // TODO consider performance overhead for streams
+        for (int i = 0; i < problemSize; i++) {
+            for (Ant ant : ants) {
+                decisionRule(ant);
+            }
+        }
     }
 
-    private void decisionRule(Ant ant, int step) {
-        final int current = ant.getCurrent();
-        //108
+    private void decisionRule(Ant ant) {
+        final int currentIndex = ant.getCurrent();
+        double sumProbabilities = IntStream.range(0, problemSize)
+                .filter(index -> !ant.isVisited(index))
+                .mapToDouble(index -> choicesInfo[currentIndex][index])
+                .reduce(0, (acc, choice) -> {
+                    return acc += choice;
+                });
+        final double randomDouble = random.nextDouble(0, sumProbabilities);
+
+        int nextIndex = 1;
+        double selectionProbability = choicesInfo[currentIndex][nextIndex];
+        for (int j = 0; j < problemSize; j++) {
+            selectionProbability += choicesInfo[currentIndex][j];
+            if (randomDouble > selectionProbability) {
+                nextIndex = j;
+                break;
+            }
+        }
+        ant.visit(nextIndex, matrices.getDistanceMatrix()[currentIndex][nextIndex]);
     }
 }
