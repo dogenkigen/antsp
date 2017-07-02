229ecf01f4d17c14eef0498114dc95c308806b64
Maciej Laskowski <mlaskowsk@gmail.com>
2017-07-02 20:17:04 +0200

    Naive version of 2-opt works

--- src/main/java/com/mlaskows/solvers/TwoOptSolver.java
+++ src/main/java/com/mlaskows/solvers/TwoOptSolver.java
@@ -1,7 +1,6 @@
 package com.mlaskows.solvers;
 
 import com.mlaskows.datamodel.Solution;
-import com.mlaskows.datamodel.Step;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,7 +13,7 @@ public class TwoOptSolver implements Solver {
 
     private final Solution initialSolution;
     private final int[][] distanceMatrix;
-    private List<Step> steps;
+    private List<Integer> tour;
 
     public TwoOptSolver(Solution initialSolution, int[][] distanceMatrix) {
         if (initialSolution.getTour().size() != distanceMatrix.length) {
@@ -23,72 +22,59 @@ public class TwoOptSolver implements Solver {
         }
         this.distanceMatrix = distanceMatrix;
         this.initialSolution = initialSolution;
-        steps = getSteps(initialSolution.getTour());
+        tour = initialSolution.getTour();
     }
 
     @Override
     public Solution getSolution() {
-        int bestDistance = getTotalDistance(steps);
-        boolean canBeImproved = true;
-        while (canBeImproved) {
-            final int improvedDistance = tryImproveDistance(bestDistance);
-            canBeImproved = improvedDistance < bestDistance;
+        int bestDistance = getDistance(tour);
+        boolean wasImproved = true;
+        while (wasImproved) {
+            final int newDistance = tryImproveDistance(bestDistance);
+            wasImproved = newDistance < bestDistance;
+            if (wasImproved) {
+                bestDistance = newDistance;
+            }
         }
 
-        return new Solution(getTour(steps), bestDistance);
+        return new Solution(tour, bestDistance);
     }
 
     private int tryImproveDistance(int bestDistance) {
-        int distance = 0;
-        for (int i = 0; i < steps.size() - 1; i++) {
-            for (int k = i + 1; k < steps.size(); k++) {
-                final List<Step> swappedSteps = twOptSwap(steps, i, k);
-                distance = getTotalDistance(swappedSteps);
-                if (distance < bestDistance) {
-                    steps = swappedSteps;
-                    break;
+        int newDistance = 0;
+        outer : for (int from = 1; from < tour.size() - 1; from++) {
+            for (int to = from + 1; to < tour.size(); to++) {
+                final List<Integer> newTour = twOptSwap(tour, from, to);
+                newDistance = getDistance(newTour);
+                if (newDistance < bestDistance) {
+                    tour = newTour;
+                    break outer;
                 }
             }
         }
+        return newDistance;
+    }
+
+
+    private List<Integer> twOptSwap(List<Integer> tour, int from, int to) {
+        final List<Integer> swappedTour = new ArrayList<>(tour.size());
+        swappedTour.addAll(tour.subList(0, from));
+        final List<Integer> fromTo = new ArrayList<>(tour.subList(from, to + 1));
+        Collections.reverse(fromTo);
+        swappedTour.addAll(fromTo);
+        swappedTour.addAll(tour.subList(to + 1, tour.size()));
+        return swappedTour;
+    }
+
+    private int getDistance(List<Integer> tour) {
+        return getDistance(tour, 0, tour.size() - 1);
+    }
+
+    private int getDistance(List<Integer> tour, int from, int to) {
+        int distance = 0;
+        for (int i = from; i < to; i++) {
+            distance += distanceMatrix[tour.get(i)][tour.get(i + 1)];
+        }
         return distance;
-    }
-
-    private List<Step> twOptSwap(List<Step> steps, int i, int k) {
-        final List<Step> swappedSteps = new ArrayList<>(steps.size());
-        final int toIndex = i - 1 >= 1 ? i - 1 : 1;
-        swappedSteps.addAll(steps.subList(1, toIndex));
-        final List<Step> iToK = steps.subList(i, k);
-        Collections.reverse(iToK);
-        swappedSteps.addAll(iToK);
-        swappedSteps.addAll(steps.subList(k + 1, steps.size() - 1));
-        return swappedSteps;
-    }
-
-    private int getTotalDistance(List<Step> steps) {
-        int ret = 0;
-        for (Step step : steps) {
-            ret += step.getDistance();
-        }
-        return ret;
-    }
-
-    private List<Step> getSteps(List<Integer> tour) {
-        final List<Step> steps = new ArrayList<>(tour.size());
-        for (int i = 0; i < tour.size() - 1; i++) {
-            steps.add(new Step(i, tour.get(i + 1),
-                    distanceMatrix[tour.get(i)][tour.get(i + 1)]));
-        }
-        return steps;
-    }
-
-    private List<Integer> getTour(List<Step> steps) {
-        List<Integer> tour = new ArrayList<>(initialSolution.getTour().size());
-        for (int i = 0; i < steps.size(); i++) {
-            tour.add(steps.get(i).getFrom());
-            if (i == steps.size() - 1) {
-                tour.add(steps.get(i).getTo());
-            }
-        }
-        return tour;
     }
 }
