4275253eed40005772853e069849f8b8b2db0924
Maciej Laskowski <mlaskowsk@gmail.com>
2017-07-22 21:34:44 +0200

    Method change

--- src/main/java/com/mlaskows/config/AcoConfigFactory.java
+++ src/main/java/com/mlaskows/config/AcoConfigFactory.java
@@ -7,31 +7,31 @@ public class AcoConfigFactory {
 
     public static AcoConfig createDefaultAntSystemConfig(int problemSize) {
         // TODO NN can be between 15 and 40. Consider options
-        return createAntSystemConfig(3, 1, 0.5, problemSize, 15, 10);
+        return createAcoConfig(3, 1, 0.5, problemSize, 15, 10);
     }
 
     public static AcoConfig createDefaultMinMaxConfig(int problemSize) {
-        return createAntSystemConfig(3, 1, 0.1, problemSize, 15, 60, 2);
+        return createAcoConfig(3, 1, 0.1, problemSize, 15, 60, 2);
     }
 
-    public static AcoConfig createAntSystemConfig(int heuristicImportance,
-                                                  int pheromoneImportance,
-                                                  double pheromoneEvaporationFactor,
-                                                  int problemSize,
-                                                  int nearestNeighbourFactor,
-                                                  int maxStagnationCount) {
+    public static AcoConfig createAcoConfig(int heuristicImportance,
+                                            int pheromoneImportance,
+                                            double pheromoneEvaporationFactor,
+                                            int problemSize,
+                                            int nearestNeighbourFactor,
+                                            int maxStagnationCount) {
         return new AcoConfig(heuristicImportance, pheromoneImportance,
                 pheromoneEvaporationFactor, problemSize, nearestNeighbourFactor,
                 maxStagnationCount);
     }
 
-    public static AcoConfig createAntSystemConfig(int heuristicImportance,
-                                                  int pheromoneImportance,
-                                                  double pheromoneEvaporationFactor,
-                                                  int problemSize,
-                                                  int nearestNeighbourFactor,
-                                                  int maxStagnationCount,
-                                                  double minPheromoneLimitDivider) {
+    public static AcoConfig createAcoConfig(int heuristicImportance,
+                                            int pheromoneImportance,
+                                            double pheromoneEvaporationFactor,
+                                            int problemSize,
+                                            int nearestNeighbourFactor,
+                                            int maxStagnationCount,
+                                            double minPheromoneLimitDivider) {
         return new AcoConfig(heuristicImportance, pheromoneImportance,
                 pheromoneEvaporationFactor, problemSize, nearestNeighbourFactor,
                 maxStagnationCount, minPheromoneLimitDivider);
