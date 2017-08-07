4ed28770e3e1792cce075777dc621faf0edbf3f7
Maciej Laskowski <mlaskowsk@gmail.com>
2017-08-07 05:14:51 +0200

    Adding some comments and tests

--- src/test/java/com/mlaskows/tsplib/TspLibParserTest.java
+++ src/test/java/com/mlaskows/tsplib/TspLibParserTest.java
@@ -4,11 +4,12 @@ import com.mlaskows.tsplib.datamodel.Tsp;
 import com.mlaskows.tsplib.types.DisplayDataType;
 import com.mlaskows.tsplib.types.EdgeWeightType;
 import com.mlaskows.tsplib.types.Type;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by mlaskows on 21/04/2017.
@@ -17,34 +18,43 @@ public class TspLibParserTest {
 
     @Test
     public void testUsa13509() throws IOException {
+        Tsp tsp = getTsp("usa13509.tsp");
 
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("usa13509.tsp").getFile());
-
-        Tsp tsp = TspLibParser.parse(file.getAbsolutePath());
-
-        Assert.assertEquals(tsp.getName(), "usa13509");
-        Assert.assertEquals(tsp.getDimension(), 13509);
-        Assert.assertEquals(tsp.getType(), Type.TSP);
-        Assert.assertEquals(tsp.getEdgeWeightType(), EdgeWeightType.EUC_2D);
-        Assert.assertEquals(tsp.getNodes().size(), 13509);
+        assertEquals(tsp.getName(), "usa13509");
+        assertEquals(tsp.getDimension(), 13509);
+        assertEquals(tsp.getType(), Type.TSP);
+        assertEquals(tsp.getEdgeWeightType(), EdgeWeightType.EUC_2D);
+        assertEquals(tsp.getNodes().size(), 13509);
     }
 
     @Test
-    public void testali535() throws IOException {
+    public void testAli535() throws IOException {
+        Tsp tsp = getTsp("ali535.tsp");
+
+        assertEquals(tsp.getName(), "ali535");
+        assertEquals(tsp.getDimension(), 535);
+        assertEquals(tsp.getType(), Type.TSP);
+        assertEquals(tsp.getEdgeWeightType(), EdgeWeightType.GEO);
+        assertEquals(tsp.getDisplayDataType(), DisplayDataType.COORD_DISPLAY);
+        assertEquals(tsp.getNodes().size(), 535);
+        assertEquals(tsp.getComment(), "535 Airports around the globe (Padberg/Rinaldi)");
+    }
+
+    @Test
+    public void testBerlin52() throws IOException {
+        final Tsp tsp = getTsp("berlin52.tsp");
+
+        assertEquals(tsp.getName(), "berlin52");
+        assertEquals(tsp.getType(), Type.TSP);
+        assertEquals(tsp.getComment(), "52 locations in Berlin (Groetschel)");
+        assertEquals(tsp.getDimension(), 52);
+        assertEquals(tsp.getEdgeWeightType(), EdgeWeightType.EUC_2D);
+    }
+
+    private Tsp getTsp(String name) throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("ali535.tsp").getFile());
-
-        Tsp tsp = TspLibParser.parse(file.getAbsolutePath());
-
-        Assert.assertEquals(tsp.getName(), "ali535");
-        Assert.assertEquals(tsp.getDimension(), 535);
-        Assert.assertEquals(tsp.getType(), Type.TSP);
-        Assert.assertEquals(tsp.getEdgeWeightType(), EdgeWeightType.GEO);
-        Assert.assertEquals(tsp.getDisplayDataType(), DisplayDataType.COORD_DISPLAY);
-        Assert.assertEquals(tsp.getNodes().size(), 535);
-        Assert.assertEquals(tsp.getComment(), "535 Airports around the globe (Padberg/Rinaldi)");
-
+        File file = new File(classLoader.getResource(name).getFile());
+        return TspLibParser.parse(file.getAbsolutePath());
     }
 
 }
