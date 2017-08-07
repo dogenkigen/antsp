5d4b9428c6f5e60475d06af863aa04c228257623
Maciej Laskowski <mlaskowsk@gmail.com>
2017-08-07 07:27:00 +0200

    Adding att test

--- src/test/java/com/mlaskows/solvers/NearestNeighbourSolverTest.java
+++ src/test/java/com/mlaskows/solvers/NearestNeighbourSolverTest.java
@@ -1,15 +1,16 @@
 package com.mlaskows.solvers;
 
+import com.mlaskows.datamodel.Solution;
 import com.mlaskows.datamodel.matrices.StaticMatricesBuilder;
 import com.mlaskows.datamodel.matrices.StaticMatricesHolder;
-import com.mlaskows.datamodel.Solution;
 import com.mlaskows.solvers.heuristic.NearestNeighbourSolver;
 import com.mlaskows.tsplib.datamodel.Tsp;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.List;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 /**
  * Created by mlaskows on 18/06/2017.
@@ -23,15 +24,22 @@ public class NearestNeighbourSolverTest implements SolverTest {
 
         final List<Integer> expectedTour = List.of(0, 2, 1, 4, 5, 3);
 
-        Assert.assertEquals(solution.getTour(), expectedTour);
-        Assert.assertEquals(solution.getTourLength(), 6095);
+        assertEquals(solution.getTour(), expectedTour);
+        assertEquals(solution.getTourLength(), 6095);
     }
 
     @Test
     public void testAli535Solution() throws IOException {
         final Solution solution = getSolution("ali535.tsp");
 
-        Assert.assertEquals(solution.getTourLength(), 224358);
+        assertEquals(solution.getTourLength(), 224358);
+    }
+
+    @Test
+    public void testAtt532() throws IOException {
+        final Solution solution = getSolution("att532.tsp");
+
+        assertEquals(solution.getTourLength(), 33470);
     }
 
     private Solution getSolution(String fileName) throws IOException {
--- src/test/java/com/mlaskows/solvers/antsolvers/AntSystemSolverTest.java
+++ src/test/java/com/mlaskows/solvers/antsolvers/AntSystemSolverTest.java
@@ -57,4 +57,24 @@ public class AntSystemSolverTest implements SolverTest {
         Assert.assertTrue(solution.getTourLength() < 224358);
     }
 
+    @Test
+    public void testAtt532Solution() throws IOException {
+        final Tsp tsp = getTsp("att532.tsp");
+        final AcoConfig config =
+                AcoConfigFactory.createDefaultAntSystemConfig(tsp.getDimension());
+        final StaticMatricesHolder matrices = new StaticMatricesBuilder(tsp)
+                .withHeuristicInformationMatrix()
+                .withNearestNeighbors(config.getNearestNeighbourFactor())
+                .build();
+        final AntSystemSolver solver = new AntSystemSolver(matrices, config);
+        final Solution solution = solver.getSolution();
+
+        List<Integer> nonImprovementPeriods = solver.getStatistics().getNonImprovementPeriods();
+        Assert.assertEquals((int) nonImprovementPeriods.get(nonImprovementPeriods.size() - 1),
+                config.getMaxStagnationCount());
+        // We assume here that solution will be better than for nearest
+        // neighbour algorithm.
+        Assert.assertTrue(solution.getTourLength() < 33470);
+    }
+
 }
