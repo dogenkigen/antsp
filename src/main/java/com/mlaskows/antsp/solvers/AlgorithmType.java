3d87706d5bef4b87e372b6899787a6549fc549a1
Maciej Laskowski <mlaskowsk@gmail.com>
2017-08-16 05:16:48 +0200

    Add antbased algorithms to type

--- src/main/java/com/mlaskows/antsp/solvers/AlgorithmType.java
+++ src/main/java/com/mlaskows/antsp/solvers/AlgorithmType.java
@@ -1,5 +1,7 @@
 package com.mlaskows.antsp.solvers;
 
+import java.util.EnumSet;
+
 public enum AlgorithmType {
 
     ANT_SYSTEM("Ant System"),
@@ -7,13 +9,21 @@ public enum AlgorithmType {
     RANK_BASED("Rank Based Ant System"),
     ELITIST("Elitist Ant System");
 
+    private static final EnumSet<AlgorithmType> antBased =
+            EnumSet.of(ANT_SYSTEM, MIN_MAX, RANK_BASED, ELITIST);
+
     private final String text;
 
     AlgorithmType(final String text) {
         this.text = text;
     }
 
+    public boolean isAntBased() {
+        return antBased.contains(this);
+    }
+
     @Override
+
     public String toString() {
         return text;
     }
