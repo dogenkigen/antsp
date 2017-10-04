f0cd597097e0bedddf4b487176477e49b267c251
dogenkigen <mlaskowsk@gmail.com>
2017-10-04 13:29:48 +0200

    Simplify RanBased deposit

--- src/main/java/com/mlaskows/antsp/solvers/antsolvers/util/pheromone/deposit/RankBasedDepositBehaviour.java
+++ src/main/java/com/mlaskows/antsp/solvers/antsolvers/util/pheromone/deposit/RankBasedDepositBehaviour.java
@@ -18,18 +18,15 @@ public class RankBasedDepositBehaviour implements DepositBehaviour {
     @Override
     public void deposit(PheromoneProcessor pheromoneProcessor, IterationResult iterationResult) {
         final Ant bestAntSoFar = iterationResult.getBestAntSoFar();
-        final List<Ant> ants = iterationResult.getSortedAnts()
-                .stream()
-                .limit(config.getWeight() - 1)
-                .collect(Collectors.toList());
-        for (int r = 0; r < ants.size(); r++) {
+        final List<Ant> ants = iterationResult.getSortedAnts();
+        final int weight = config.getWeight();
+        for (int r = 0; r < weight; r++) {
             final Ant ant = ants.get(r);
-            final double pheromoneDelta = (double) (config.getWeight() - r)
+            final double pheromoneDelta = (double) (weight - r)
                     / ant.getTourLength();
             pheromoneProcessor.depositAntPheromone(ant, pheromoneDelta);
         }
-        pheromoneProcessor.depositAntPheromone(bestAntSoFar, (double) config
-                .getWeight()
+        pheromoneProcessor.depositAntPheromone(bestAntSoFar, (double) weight
                 / bestAntSoFar.getTourLength());
     }
 }
