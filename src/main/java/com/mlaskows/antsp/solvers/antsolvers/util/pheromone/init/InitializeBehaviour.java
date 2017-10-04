bc5d0b732014c629323cd8fbaa2658a1af9433d8
dogenkigen <mlaskowsk@gmail.com>
2017-10-04 13:46:04 +0200

    Adding TODO

--- src/main/java/com/mlaskows/antsp/solvers/antsolvers/util/pheromone/init/InitializeBehaviour.java
+++ src/main/java/com/mlaskows/antsp/solvers/antsolvers/util/pheromone/init/InitializeBehaviour.java
@@ -7,10 +7,11 @@ import com.mlaskows.antsp.solvers.antsolvers.util.pheromone.PheromoneProcessor;
 import com.mlaskows.antsp.solvers.heuristic.NearestNeighbourSolver;
 
 public interface InitializeBehaviour {
-
+    //TODO config param can be removed and passed in constructors.
+    //TODO This will make possible getting rid of casting in some classes
     void initialize(PheromoneProcessor pheromoneProcessor,
-                                    StaticMatrices matrices,
-                                    AcoConfig config);
+                    StaticMatrices matrices,
+                    AcoConfig config);
 
     default Solution getNearestNeighbourSolution(StaticMatrices matrices) {
         final NearestNeighbourSolver nearestNeighbourSolver =
