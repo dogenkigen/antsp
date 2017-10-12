ae6ce77e4fae18f068c75bebbff7117a72ff9bb4
dogenkigen <mlaskowsk@gmail.com>
2017-10-12 19:38:39 +0200

    Fix ranked-based deposit for small amount of ants

--- src/main/java/com/mlaskows/antsp/solvers/antsolvers/util/pheromone/deposit/RankBasedDepositBehaviour.java
+++ src/main/java/com/mlaskows/antsp/solvers/antsolvers/util/pheromone/deposit/RankBasedDepositBehaviour.java
@@ -20,7 +20,7 @@ public class RankBasedDepositBehaviour implements DepositBehaviour {
         final Ant bestAntSoFar = iterationResult.getBestAntSoFar();
         final List<Ant> ants = iterationResult.getSortedAnts();
         final int weight = config.getWeight();
-        for (int r = 0; r < weight; r++) {
+        for (int r = 0; r < weight && r < ants.size(); r++) {
             final Ant ant = ants.get(r);
             final double pheromoneDelta = (double) (weight - r)
                     / ant.getTourLength();
