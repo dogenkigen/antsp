ff805b1139884330d94a700f1c6fd2242ac6a438
Maciej Laskowski <mlaskowsk@gmail.com>
2017-10-17 18:21:17 +0200

    Revert "Introducing IterationResultBehaviour"
    This reverts commit 55307a9ea83eb397023e51983733553a659688e0.

--- src/main/java/com/mlaskows/antsp/solvers/antsolvers/MaxMinAntSolver.java
+++ src/main/java/com/mlaskows/antsp/solvers/antsolvers/MaxMinAntSolver.java
@@ -2,14 +2,13 @@ package com.mlaskows.antsp.solvers.antsolvers;
 
 import com.mlaskows.antsp.config.MaxMinConfig;
 import com.mlaskows.antsp.datamodel.matrices.StaticMatrices;
-import com.mlaskows.antsp.solvers.antsolvers.util.ant.IterationResultBehaviour;
 import com.mlaskows.antsp.solvers.antsolvers.util.ant.IterationResultFactory;
 import com.mlaskows.antsp.solvers.antsolvers.util.pheromone.MaxMinPheromoneBehaviour;
 
 public class MaxMinAntSolver extends GenericAntSolver {
 
     public MaxMinAntSolver(StaticMatrices matrices, MaxMinConfig config) {
-        super(config, new IterationResultBehaviour(matrices, config),
+        super(config, new IterationResultFactory(matrices, config),
                 new MaxMinPheromoneBehaviour(matrices, config));
     }
 
